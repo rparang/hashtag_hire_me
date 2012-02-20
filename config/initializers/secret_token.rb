6097fe41d45297441f11ebc3817930b6768777b4
rparang <reza@vitrue.com>
2012-02-20 02:03:09 -0500

    initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectOne::Application.config.secret_token = 'aad4d945a98ab557bc8933c24afc2ca472afe682674f2f2318634b4fde96e12826162885c0cf2e9edfe8bddf16d085112ed268e5b96b45e99b38b8b39dc16b99'
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+ProjectOne::Application.routes.draw do
+  root :to => 'pages#home'
+  
+  #match '/pages',    :to => 'pages#home'
+
+end
